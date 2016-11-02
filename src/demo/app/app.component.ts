21ffb17432b8f58a3275dae06f28a29e5f1c9bca
Tine Kondo <kondotine@gmail.com>
2016-11-02 18:56:45 +0100

    chore(scss): change style type from 'css' to 'scss'

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -4,7 +4,7 @@ import { NgsRevealConfig } from './../../lib/ngs-reveal-config';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
