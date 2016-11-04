498f2567d55feeb90355d0c5684a80d88a4c5734
Tine Kondo <kondotine@gmail.com>
2016-11-04 07:51:04 +0100

    chore(demo): add 'home' and 'getting-started' pages

---
+++ src/demo/app/app-routing.ts
@@ -0,0 +1,28 @@
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { GettingStartedComponent } from './getting-started/getting-started.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'getting-started',
+        component: GettingStartedComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
