861dc5fd6d8704942d5849ee2e18ce825b55e96f
Tine Kondo <kondotine@gmail.com>
2016-11-04 07:50:18 +0100

    chore(demo): add shared modules (header, footer, content-wrapper)

---
+++ src/demo/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+
+@NgModule({
+    imports: [RouterModule],
+    exports: [HeaderComponent, FooterComponent, ContentWrapperComponent],
+    declarations: [HeaderComponent, FooterComponent, ContentWrapperComponent],
+    providers: [],
+})
+export class AppSharedModule { }
