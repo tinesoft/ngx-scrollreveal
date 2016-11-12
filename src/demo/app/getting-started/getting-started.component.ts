0c6c4295f11c4d061981ca3bfd8f5fc01b49f98b
Tine Kondo <tinesoft@MBP-de-Tine.fritz.box>
2016-11-12 04:07:29 +0100

    chore(demo): set page title according to routes

--- src/demo/app/getting-started/getting-started.component.ts
+++ src/demo/app/getting-started/getting-started.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-getting-started',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GettingStartedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService:Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Getting Started | ng2-scrollreveal');
   }
 
 }
--- src/demo/app/home/home.component.ts
+++ src/demo/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService:Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle('Home | ng2-scrollreveal');
   }
 
 }
