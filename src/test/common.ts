4f81cf1de57739f2c67fc8604321f517caea1a38
Tine Kondo <kondotine@gmail.com>
2016-11-18 02:15:23 +0100

    chore(cleanup): remove unsed code

--- src/test/common.ts
+++ src/test/common.ts
@@ -3,14 +3,6 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { Predicate } from '@angular/core/src/facade/collection';
 
 
-export function createGenericTestComponent<T>(html: string, type: { new (...args: any[]): T }): ComponentFixture<T> {
-    TestBed.overrideComponent(type, { set: { template: html } });
-    const fixture = TestBed.createComponent(type);
-    fixture.detectChanges();
-    return fixture as ComponentFixture<T>;
-}
-
-
 export function not<T>(predicate: Predicate<T>): Predicate<T> {
     return function () {
         return !predicate.apply(this, arguments);
@@ -23,8 +15,3 @@ export function and<T>(predicate1: Predicate<T>, predicate2: Predicate<T>): Pred
     };
 }
 
-export function or<T>(predicate1: Predicate<T>, predicate2: Predicate<T>): Predicate<T> {
-    return function () {
-        return predicate1.apply(this, arguments) || predicate2.apply(this, arguments);
-    };
-}
