0f5b9a3fab9cbaa12e8dda1ffbe766b700bf131e
Tine Kondo <kondotine@gmail.com>
2018-09-13 05:32:01 +0200

    feat(core): update to `Angular v6.x.x` and `generator-ngx-library@6.2.1`

--- demo/src/app/app.server.module.ts
+++ demo/src/app/app.server.module.ts
@@ -1,9 +1,9 @@
-import {NgModule} from '@angular/core';
-import {ServerModule} from '@angular/platform-server';
-
-import {AppModule} from './app.module';
-import {AppComponent} from './app.component';
+import { NgModule } from '@angular/core';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
@@ -11,7 +11,8 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
     // by the ServerModule from @angular/platform-server.
     AppModule,
     ServerModule,
-    ModuleMapLoaderModule
+    ModuleMapLoaderModule,
+    ServerTransferStateModule,
   ],
   // Since the bootstrapped component is not inherited from your
   // imported AppModule, it needs to be repeated here.
