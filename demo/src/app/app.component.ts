12d4da87db3ab02503edb7637a9a9b84d1f52989
Tine Kondo <kondotine@gmail.com>
2018-09-17 22:22:13 +0200

    feat(core): migrate to ScrollReveal `v4.x.x`
    - add `desktop` option in `NgsRevealConfig`
    - add `beforeReveal$`, `afterReveal$`, `beforeReset$`, `afterReset$` observables in `NgsRevealService` to subscribe to these events (see usage [here](https://github.com/tinesoft/ng-scrollreveal#subscribing-to-scrollreveal-events))
    
    BREAKING CHANGE: Here are the breaking changes, mostly coming from ScrollReveal:
    
    - `@types/scrollreveal` is no longer necessary and **must be removed** from your `package.son`
    - `options.distance` now only supports `em`, `px` and `%` values
    - `NgsRevealConfig` default values have been updated to match [ScrollReveal](https://scrollrevealjs.org/api/defaults.html)'s
    - be aware that `ScrollReveal` now requires a **commercial license**, unless for [GPL-3.0](https://opensource.org/licenses/GPL-3.0) compatible open source projects.

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd, RouterEvent } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { NgsRevealService } from 'ng-scrollreveal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
 
-  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {
+  //keep refs to subscriptions to be able to unsubscribe later
+  private beforeRevealSubscription: Subscription;
+  private afterRevealSubscription: Subscription;
+  private beforeResetSubscription: Subscription;
+  private afterResetSubscription: Subscription;
+
+  constructor(private revealService: NgsRevealService, private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {
 
     this.router.events.pipe(
       filter((event:RouterEvent) => event instanceof NavigationEnd)
@@ -19,5 +27,36 @@ export class AppComponent {
         window.scroll(0, 0);
       }
     });
+  }
+
+  ngOnInit() {
+    // subscribe to ScrollReveal observables to react to main events
+    this.beforeRevealSubscription = this.revealService.beforeReveal$.subscribe(
+      (el: HTMLElement) => {
+        console.log(`beforeReveal of '<${el.nodeName}>.${el.className}'`);
+      });
+
+    this.afterRevealSubscription = this.revealService.afterReveal$.subscribe(
+      (el: HTMLElement) => {
+        console.log(`afterReveal of '<${el.nodeName}>.${el.className}'`);
+    });
+
+    this.beforeResetSubscription = this.revealService.beforeReset$.subscribe(
+      (el: HTMLElement) => {
+        console.log(`beforeReset of '<${el.nodeName}>.${el.className}'`);
+    });
+
+    this.afterResetSubscription = this.revealService.afterReset$.subscribe(
+      (el: HTMLElement) => {
+        console.log(`afterReset of '<${el.nodeName}>.${el.className}'`);
+    });
+  }
+
+  ngOnDestroy() {
+    // unsubscribe to ScrollReveal observables to prevent memory leaks
+    this.beforeRevealSubscription.unsubscribe();
+    this.afterRevealSubscription.unsubscribe();
+    this.beforeResetSubscription.unsubscribe();
+    this.afterResetSubscription.unsubscribe();
   }
 }
--- src/services/ngs-reveal-config.spec.ts
+++ src/services/ngs-reveal-config.spec.ts
@@ -5,15 +5,16 @@ describe('ngs-reveal-config', () => {
         const config = new NgsRevealConfig();
 
         expect(config.origin).toBe('bottom');
-        expect(config.distance).toBe('20px');
-        expect(config.duration).toBe(500);
+        expect(config.distance).toBe('0px');
+        expect(config.duration).toBe(600);
         expect(config.delay).toBe(0);
         expect(config.rotate).toEqual({ x: 0, y: 0, z: 0 });
         expect(config.opacity).toBe(0);
         expect(config.scale).toBe(0.9);
-        expect(config.easing).toBe('cubic-bezier(0.6, 0.2, 0.1, 1)');
+        expect(config.easing).toBe('cubic-bezier(0.5, 0, 0, 1)');
         expect(config.container).toBeUndefined();
         expect(config.mobile).toBe(true);
+        expect(config.desktop).toBe(true);
         expect(config.reset).toBe(false);
         expect(config.useDelay).toBe('always');
         expect(config.viewFactor).toBe(0.2);
